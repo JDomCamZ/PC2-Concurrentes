f1061aeb0f2bc9848568f76e03935bb63610c265
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-10-25 02:00:11 -0500

    update

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,14 +1,11 @@
-
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ArmandoLluen <armando.lluen.g@uni.pe>
